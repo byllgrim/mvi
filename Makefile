5b91ff354125a772652eebf5ca53c131f38eff46
byllgrim <robin.a.t.pedersen@gmail.com>
2016-11-05 19:41:34 +0100

    enable leftwards movements
    hjkl should work as arrow keys. h now works partially.
    It is very naïve and poor and the moment, but the groundworks should be laid.
    
    Also, theres a lot of TODOs scattered around the source code.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CPPFLAGS = -D_POSIX_C_SOURCE=200112L
 CFLAGS = -std=c89 -pedantic-errors -Wall -Wextra -Os ${CPPFLAGS}
-LDFLAGS = -lcurses -lutf
+LDFLAGS = -lncursesw -lutf
 
 all:
 	cc -o mvi ${CFLAGS} mvi.c ${LDFLAGS}
--- mvi.c
+++ mvi.c
@@ -1,5 +1,7 @@
 /* See LICENSE file */
 #include <curses.h>
+#include <locale.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <utf.h>
@@ -7,6 +9,7 @@
 /* macros */
 #define ISESC(c) (c == 27)
 /* TODO VLEN use utflen? */
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
 
 /* types */
 typedef enum {
@@ -31,15 +34,19 @@ typedef struct {
 } Cursor;
 
 /* function declarations */
-static void cmdinsert(void);
+static void cmdinsert(void); /* TODO change name */
 static void cmdnormal(void); /* TODO change name? */
 static void draw(void);
-static void insertch(int r);
+static void insertch(int c);
 static void insertstr(char *str);
 static Line *newline(void); /* TODO arguments next/prev */
+static void moveleft(void);
+static int prevlen(char *s, int o); /* TODO dont need arguments? */
+static void tmpstatus(char *fmt, ...); /* TODO temporary? */
 
 /* global variables */
 static int edit = 1;
+static int cury, curx; /* TODO rename y,x or posy,posx */
 static Cursor cur; /* TODO be mindful of the stack */
 /* TODO firstline? */
 static Mode mode = NORMAL;
@@ -52,7 +59,7 @@ cmdinsert(void)
 
 	c = getch();
 	if (ISESC(c))
-		mode = NORMAL;
+		mode = NORMAL; /* TODO moveleft()? */
 	else
 		insertch(c);
 }
@@ -61,6 +68,9 @@ void
 cmdnormal(void)
 {
 	switch(getch()) {
+	case 'h':
+		moveleft();
+		break;
 	case 'i':
 		mode = INSERT;
 		break;
@@ -73,37 +83,47 @@ cmdnormal(void)
 void
 draw(void)
 {
-	/* TODO print all lines */
-	mvprintw(0, 0, "%s", cur.l->s); /* TODO only temporary */
+	mvprintw(0, 0, "%s", cur.l->s); /* TODO print all lines */
+	move(cury, curx);
 	refresh();
 }
 
 void
-insertch(int r)
+insertch(int c) /* TODO change variable name */
 {
 	int i;
 	char *s;
 
+	if (KEY_MIN <= c && c <= KEY_MAX) /* curses special keys */
+		return; /* TODO is it necessary? */
+
 	s = calloc(5, sizeof(char));
-	s[0] = r;
-	for (i = 1; i < 4; i++) { /* find utflen */
+	s[0] = (char)c;
+	for (i = 1; i < 4; i++) { /* find utf char */
 		if (fullrune(s, i))
 			break;
 		s[i] = getch();
         }
 	insertstr(s);
+	++curx;
 }
 
 void
 insertstr(char *src)
 {
 	size_t len = strlen(src); /* TODO trust strlen? */
-	if ((cur.l->l + len) < (BUFSIZ * cur.l->m)) { /* TODO logic */
-		strncat(cur.l->s, src, len);
-		/* TODO consider \n inside line */
-		cur.l->l += len;
-	}
-	/* TODO insert at correct position */
+	/* TODO char *ins = cur.l->s + cur.o */
+
+	if ((cur.l->l + len) >= (BUFSIZ * cur.l->m)) /* not enough space */
+		return; /* TODO make more space */
+
+	if (cur.l->s[cur.o] != '\0') /* not at end of string */
+		memmove(cur.l->s+len+cur.o, cur.l->s+cur.o, cur.l->l - cur.o);
+
+	memcpy(cur.l->s + cur.o, src, len);
+	/* TODO consider \n inside line */
+	cur.l->l += len;
+	cur.o += len;
 }
 
 Line *
@@ -120,15 +140,63 @@ newline(void)
 	return l;
 }
 
+void
+moveleft(void)
+{
+	if (!cur.o) /* is at beginning of string */
+		return;
+
+	if (!curx) /* is at beginning of termwidth */
+		return; /* TODO crawl upwards */
+
+	--curx;
+	cur.o -= prevlen(cur.l->s, cur.o);
+}
+
+int
+prevlen(char *s, int o)
+{
+	int max, i, n;
+	Rune p; /* TODO NULL */
+
+	max = MIN(UTFmax, o);
+	for (i = max; i > 0; i--) {
+		n = charntorune(&p, s+o-i, i);
+		if (n == i)
+			return n;
+	}
+	return 0; /* TODO -1 */
+}
+
+void
+tmpstatus(char *fmt, ...)
+{
+	char *msg;
+	int len;
+	va_list ap;
+
+	len = getmaxx(stdscr);
+	msg = calloc(len+1, sizeof(char));
+
+	va_start(ap, fmt);
+	vsnprintf(msg, len, fmt, ap);
+	va_end(ap);
+
+	mvprintw(LINES-3, 0, msg);
+}
+
 int
 main()
 {
+	setlocale(LC_ALL, ""); /* TODO is this necessary? */
 	initscr();
 	raw();
 	noecho();
-	/* TODO init()? */
+	cury = curx = 0;
 	cur.o = 0;
 	cur.l = newline();
+	/* TODO init()? */
+	/* TODO setlocale? */
 
 	while (edit) {
 		draw();
